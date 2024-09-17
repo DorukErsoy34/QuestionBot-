e3719eeeb97ae403278b0779258132e92da30e1c
DorukErsoy34 <doruk.ersoy@hisarschool.k12.tr>
2024-09-17 22:12:11 +0300

    Initial Commit

---
+++ QuestionBot/QuestionAnswerer.swift
@@ -0,0 +1,16 @@
+struct MyQuestionAnswerer {
+    func responseTo(question: String) -> String {
+        // TODO: Write a response
+        return "This bro is smarter than me ask him:https://openai.com/chatgpt/"
+    }
+}
+
+
+    func responseTo(question: String) -> String {
+    if question.hasPrefix("hello") {
+        return "why, hello there"
+    } else {
+        return "that really depends"
+    }
+}
+
